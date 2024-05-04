45b81cccd7e0904f2278cfaeee925f5054bcbf3d
dev desai <dev_desai@outlook.com>
2024-05-04 14:30:26 -0400

    created database client

--- backend/img2vec.py
+++ backend/img2vec.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageEnhance
 from keras.api.applications.vgg19 import VGG19
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-import os
+import io
 import base64
 
 class Img2Vec:
@@ -17,13 +17,37 @@ class Img2Vec:
     for model_layer in self.vgg19.layers:
         model_layer.trainable = False
   
-  def load_image(self, image_path):
+  def load_local_image(self, image_path):
     """
     Takes in a local image path and returns a (224, 224, 3) 
     numpy array of the image
     """
     # Open the image file and resize the image to 224x224
     input_image = Image.open(image_path)
+    resized_image = input_image.resize((224, 224))
+
+    # Enhance the contrast of the image and then convert the image to a numpy array
+    contrast = ImageEnhance.Contrast(resized_image)
+    enhanced_image = contrast.enhance(1.3)
+
+    image1_array = np.array(enhanced_image)
+
+    # Add an extra dimension to the array for batch processing
+    image1_array = np.expand_dims(image1_array, axis=0)
+
+    return image1_array
+
+  def load_base64_image(self, image_base64):
+    """
+    Takes in a base64 image string and returns a (224, 224, 3) 
+    numpy array of the image
+    """
+    # Decode base64 string into bytes
+    image_bytes = base64.b64decode(image_base64)
+    image_stream = io.BytesIO(image_bytes)
+
+    # Open the image file and resize the image to 224x224
+    input_image = Image.open(image_stream)
     resized_image = input_image.resize((224, 224))
 
     # Enhance the contrast of the image and then convert the image to a numpy array
