2ee0e761cf424c9c64af690e86cd3549b891935a
Carsten <cscarsten334@gmail.com>
2024-05-04 01:24:24 -0400

    intro animation done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+import "../index.css"
+
+import Animation from './components/Animation'
+import NavBar from './components/NavBar'
+
+const Home = () => {
+    const [isImageInputted, setIsImageInputted] = useState(false)
+    const [showTool, setShowTool] = useState(false)
+    const location = useLocation();
+    const isRoot = location.pathname === '/';
+
+    // Check if image is inputted
+    const handleImageInput = () => {
+        setIsImageInputted(true);
+        console.log("Image is Inputted")
+    };
+
+    useEffect(() => {
+        // Set a timer to change showTool state after 3 seconds
+        const timer = setTimeout(() => {
+            setShowTool(true);
+        }, 3000);  
+
+
+        return () => clearTimeout(timer);
+    }, [])
+
+    return (
+      <>
+        
+        {isRoot && < Animation />}
+        <NavBar />
+
+      </>
+    );
+};
+
+export default Home;
